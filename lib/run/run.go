e2d73b460d5a87bde03cd1e7075db043212839fc
Ethan Zhang <ethanz@0g.ai>
2024-10-12 05:26:35 -0500

    to broadcast the generated transactions

--- lib/run/run.go
+++ lib/run/run.go
@@ -1,16 +1,24 @@
 package run
 
 import (
-	"fmt"
 	"log"
 )
 
 func Run(rpcUrl, faucetPrivateKey string, senderCount, txCount int) {
 	generator, err := NewGenerator(rpcUrl, faucetPrivateKey, senderCount, txCount)
 	if err != nil {
-		log.Fatalf("Failed to generate generator: %v", err)
+		log.Fatalf("Failed to create generator: %v", err)
 	}
 
 	txsMap, err := generator.GenerateSimple()
-	fmt.Printf("%v", txsMap)
+
+	transmitter, err := NewTransmitter(rpcUrl)
+	if err != nil {
+		log.Fatalf("Failed to create transmitter: %v", err)
+	}
+
+	err = transmitter.Broadcast(txsMap)
+	if err != nil {
+		log.Fatalf("Failed to broadcast transactions: %v", err)
+	}
 }
